f046e7b82adc45b1e92377e293d0b2620e356493
Kevin Cantu <me@kevincantu.org>
2012-01-13 22:59:17 -0800

    observing the optimization fail, many files...

---
+++ bench.rs
@@ -0,0 +1,20 @@
+// Kevin Cantu
+// hash function testing
+
+#[link(name = "bench", author = "kcantu", vers = "0.0")];
+
+use std;
+use str2;
+
+fn word_of_god () -> [str]
+{
+   std::io::println ("Loading the lolcat bible...");
+   let path = "./lolcat/LOLCatBible_2012-01-04.xml";
+   let bible = std::io::read_whole_file (path);
+   let bible_ = str::unsafe_from_bytes (result::get (bible));
+
+   ret str2::words(bible_) +
+       vec::map(str2::lines(bible_), {|ln| ln+ln+ln+ln});
+}
+
+
---
+++ djb.rs
@@ -0,0 +1,17 @@
+// a copy of the core library's str::hash()
+
+#[link(name = "djb", vers = "0")];
+
+fn djb(&&s: str) -> uint {
+    let u: uint = 5381u;
+    for c: u8 in s { u *= 33u; u += c as uint; }
+    ret u;
+}
+
+fn djb_(&&s: str) -> [uint] {
+    let u: uint = 5381u;
+    for c: u8 in s { u *= 33u; u += c as uint; }
+    ret [u];
+}
+
+
